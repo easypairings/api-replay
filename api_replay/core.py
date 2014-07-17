f84d59c9e7f478255ab776140ca92a1a97608648
Peter Lada <peter@easypairings.com>
2014-07-16 20:03:19 -0400

    REDIS_URL is needed, message as such; also ignore peter's venv dir

--- api_replay/core.py
+++ api_replay/core.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask.ext.redis import Redis
+from os import environ as env
+import sys
 
 app = Flask(__name__)
 redis_store = Redis()
@@ -7,6 +9,9 @@ redis_store = Redis()
 
 def setup_app():
     app.config.from_object('api_replay.settings')
+    if env.get('REDIS_URL') is None:
+    	print "Please set REDIS_URL env var, for local testing it should be redis://localhost:6379"
+    	sys.exit(1) 
     redis_store.init_app(app)
     from . import views  # NOQA registers the routes
     return app
